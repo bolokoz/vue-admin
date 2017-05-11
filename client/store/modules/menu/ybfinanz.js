125dc0b808086919eaca2171aa4f51451965cf16
yuri f becker <yurifbeck@gmail.com>
2017-05-11 09:34:41 -0400

    alalalal

---
+++ client/store/modules/menu/ybfinanz.js
@@ -0,0 +1,28 @@
+import lazyLoading from './lazyLoading'
+
+export default {
+  meta: {
+    label: 'YB Finanz',
+    icon: 'fa fa-line-chart',
+    expanded: true
+  },
+
+  children: [
+    {
+      name: 'Reddit',
+      path: '/reddit',
+      meta: {
+        link: 'ybfinanz/RedditTutorial.vue'
+      },
+      component: lazyLoading('ybfinanz/RedditTutorial')
+    },
+    {
+      name: 'Stock Analysis',
+      path: '/stock',
+      meta: {
+        link: 'ybfinanz/StockAnalysis.vue'
+      },
+      component: lazyLoading('ybfinanz/StockAnalysis')
+    }
+  ]
+}
